d8d210e43825430beffdce06e7f59125d8334389
raj <torajakumar@gmail.com>
2019-04-04 20:49:29 +0800

    new repo

---
+++ src/main/java/com/reinvent/hazelcast/App.java
@@ -0,0 +1,13 @@
+package com.reinvent.hazelcast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
